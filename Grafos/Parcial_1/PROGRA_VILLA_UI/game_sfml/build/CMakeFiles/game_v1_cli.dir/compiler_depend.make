fe9b32754d1706b995e62d7cdb563fb349412cb2
Mats2208 <a2024111556@estudiantes.upsa.edu.bo>
2026-04-15 03:11:28 -0400

    feat: refactorizando por versiones

---
+++ Grafos/Parcial_1/PROGRA_VILLA_UI/game_sfml/build/CMakeFiles/game_v1_cli.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for game_v1_cli.
+# This may be replaced when dependencies are built.
---
+++ Grafos/Parcial_1/PROGRA_VILLA_UI/game_sfml/build/CMakeFiles/game_v1_sfml.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game_v1_sfml.dir/src/main_sfml.cpp.o"
+  "CMakeFiles/game_v1_sfml.dir/src/main_sfml.cpp.o.d"
+  "game_v1_sfml"
+  "game_v1_sfml.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game_v1_sfml.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
